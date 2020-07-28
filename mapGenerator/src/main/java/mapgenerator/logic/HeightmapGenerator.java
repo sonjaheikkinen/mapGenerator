fd6683831a1e431bc0572ad4d3cfccaf288469df
Sonja Heikkinen <sonja.t.heikkinen@hotmail.com>
2020-07-28 20:32:20 +0300

    Remove more comments

--- mapGenerator/src/main/java/mapgenerator/logic/HeightmapGenerator.java
+++ mapGenerator/src/main/java/mapgenerator/logic/HeightmapGenerator.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package mapgenerator.logic;
 
 import java.util.Arrays;
 import java.util.Random;
 
-/**
- *
- * @author heisonja
- */
 public class HeightmapGenerator {
 
     double[][] heights;
