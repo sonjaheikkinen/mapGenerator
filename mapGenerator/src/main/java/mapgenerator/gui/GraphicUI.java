b99835625ae77644872f621ea396ed632c166b37
Sonja Heikkinen <sonja.t.heikkinen@hotmail.com>
2020-07-28 20:31:36 +0300

    Remove comments

--- mapGenerator/src/main/java/mapgenerator/gui/GraphicUI.java
+++ mapGenerator/src/main/java/mapgenerator/gui/GraphicUI.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package mapgenerator.gui;
 
 import javafx.scene.Scene;
@@ -13,10 +9,6 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import mapgenerator.domain.Map;
 
-/**
- *
- * @author heisonja
- */
 public class GraphicUI {
 
     int[][] heightMap;
--- mapGenerator/src/main/java/mapgenerator/logic/MapConstructor.java
+++ mapGenerator/src/main/java/mapgenerator/logic/MapConstructor.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package mapgenerator.logic;
 
 import mapgenerator.domain.Map;
 
-/**
- *
- * @author heisonja
- */
 public final class MapConstructor {
 
     Map map;
