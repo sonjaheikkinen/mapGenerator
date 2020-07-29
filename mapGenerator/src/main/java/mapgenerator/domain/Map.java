cd6fea91598fb358639ccf89e97421204025311c
Sonja Heikkinen <sonja.t.heikkinen@hotmail.com>
2020-07-29 20:05:08 +0300

    Add javadoc for Map class

--- mapGenerator/src/main/java/mapgenerator/domain/Map.java
+++ mapGenerator/src/main/java/mapgenerator/domain/Map.java
@@ -1,14 +1,25 @@
 
 package mapgenerator.domain;
 
+/**
+ * Class contains all information regarding the created map.
+ */
 public class Map {
     
-    double[][] heightMap;
+    private double[][] heightMap;
     
+    /**
+     * Sets a height map.
+     * @param heightMap An array containing the height values of the map as doubles.
+     */
     public void setHeightMap(double[][] heightMap) {
         this.heightMap = heightMap;
     }
     
+    /**
+     * Returns the height map.
+     * @retun An array containing the height values of the map as doubles.
+     */
     public double[][] getHeightMap() {
         return this.heightMap;
     }
