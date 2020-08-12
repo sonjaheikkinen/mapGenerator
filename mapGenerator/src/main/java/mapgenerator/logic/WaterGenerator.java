0666712dd6dff1411fe613913f33ef74c3a90746
Sonja Heikkinen <sonja.t.heikkinen@hotmail.com>
2020-08-12 21:32:48 +0300

    Try to optimize dijkstra and make rivers wider near ocean

--- mapGenerator/src/main/java/mapgenerator/logic/WaterGenerator.java
+++ mapGenerator/src/main/java/mapgenerator/logic/WaterGenerator.java
@@ -71,36 +71,63 @@ public class WaterGenerator {
     }
 
     public void addRivers(double[][] heightmap) {
+
         for (int startX = 0; startX < heightmap.length; startX++) {
             for (int startY = 0; startY < heightmap.length; startY++) {
+
                 int randomNumber = random.nextInt(1000);
+
                 if (!water[startX][startY] && randomNumber == 1) {
-                    if (!water[startX][startY]) {
-                        Node riverEnd = dijkstra(startX, startY, heightmap);
-                        Node currentPlace = riverEnd;
+
+                    Node riverEnd = dijkstra(startX, startY, heightmap);
+                    
+                    Node currentPlace = riverEnd;
+                    int riverLength = 30;
+                    
+                    while (true) {
+                        riverLength--;
+                        Node parent = currentPlace.getParent();
+                        int x = currentPlace.getX();
+                        int y = currentPlace.getY();
                         while (true) {
-                            //Node parent = parentNodes.get(currentPlace);
-                            Node parent = currentPlace.getParent();
-                            int x = currentPlace.getX();
-                            int y = currentPlace.getY();
-                            
-                            for (int xCoord = x; xCoord <= x + 1; xCoord++) {
-                                for (int yCoord = y; yCoord <= y + 1; yCoord++) {
-                                    if (xCoord >= 0 && xCoord < mapSize && yCoord >= 0 && yCoord < mapSize) {
-                                        rivers[xCoord][yCoord] = true;
-                                    }
+                            if (x >= 0 && x < mapSize && y >= 0 && y < mapSize) {
+                                rivers[x][y] = true;
+                                if (riverLength > 10 && x < mapSize - 1 && y > 0) {
+                                    rivers[x][y - 1] = true;
+                                    rivers[x + 1][y - 1] = true;
+                                    rivers[x + 1][y] = true;
+                                }
+                                if (riverLength > 20 && x < mapSize - 1 && x > 0 && y < mapSize - 1 && y > 0) {
+                                    rivers[x + 1][y + 1] = true;
+                                    rivers[x][y + 1] = true;
+                                    rivers[x - 1][y + 1] = true;
+                                    rivers[x - 1][y] = true;
+                                    rivers[x - 1][y - 1] = true;
                                 }
                             }
-                            
-                            currentPlace = parent;
-                            if (parent.getX() == startX && parent.getY() == startY) {
+                            if (parent.getX() < x) {
+                                x--;
+                            } else if (parent.getX() > x) {
+                                x++;
+                            }
+                            if (parent.getY() < y) {
+                                y--;
+                            } else if (parent.getY() > y) {
+                                y++;
+                            }
+                            if (x == parent.getX() && y == parent.getY()) {
                                 break;
                             }
+                        }
+                        currentPlace = parent;
+                        if (parent.getX() == startX && parent.getY() == startY) {
+                            break;
                         }
                     }
                 }
             }
         }
+        
         for (int x = 0; x < mapSize; x++) {
             for (int y = 0; y < mapSize; y++) {
                 if (rivers[x][y]) {
@@ -111,8 +138,9 @@ public class WaterGenerator {
     }
 
     public Node dijkstra(int x, int y, double[][] heightmap) {
+
+        //Initialize
         this.visited = new boolean[mapSize][mapSize];
-        //this.parentNodes = new HashMap<>();
         this.distance = new double[mapSize][mapSize];
         this.priorityQ = new PriorityQueue<>();
         for (int row = 0; row < mapSize; row++) {
@@ -120,20 +148,37 @@ public class WaterGenerator {
                 this.distance[row][column] = Integer.MAX_VALUE;
             }
         }
+
+        //Set start
         Node start = new Node(x, y, 0);
-        //parentNodes.put(start, start);
         start.setParent(start);
         priorityQ.add(start);
         distance[x][y] = 0;
         Node place = start;
+
+        //Find route
         while (!priorityQ.isEmpty()) {
+
+            //Get place
             place = priorityQ.poll();
+
+            //If place not yet visited, update distances
             if (!visited[place.getX()][place.getY()]) {
+
+                //set place to visited
                 visited[place.getX()][place.getY()] = true;
-                for (int xCoord = place.getX() - 1; xCoord <= place.getX() + 1; xCoord++) {
-                    for (int yCoord = place.getY() - 1; yCoord <= place.getY() + 1; yCoord++) {
+
+                //Go trough neighbors
+                for (int xCoord = place.getX() - 2; xCoord <= place.getX() + 2; xCoord = xCoord + 2) {
+                    for (int yCoord = place.getY() - 2; yCoord <= place.getY() + 2; yCoord = yCoord + 2) {
+
+                        //Only update neighbors that are in map
                         if (xCoord >= 0 && xCoord < mapSize && yCoord >= 0 && yCoord < mapSize) {
+
+                            //get current distance of the neighbor
                             double currentNeighborDistance = distance[xCoord][yCoord];
+
+                            //calculate distance of the neighbor if going trough current place
                             double heightDifferenceToNeighbor = heightmap[xCoord][yCoord]
                                     - heightmap[place.getX()][place.getY()];
                             double distanceFromPlaceToNeighbor = heightDifferenceToNeighbor;
@@ -141,13 +186,15 @@ public class WaterGenerator {
                                 distanceFromPlaceToNeighbor = 4 * Math.abs(distanceFromPlaceToNeighbor);
                             }
                             double newNeighborDistance = place.getDistance() + distanceFromPlaceToNeighbor;
+
+                            //if distance smaller, set current place as parent and add neighbor to priorityqueue
                             if (newNeighborDistance < currentNeighborDistance) {
                                 distance[xCoord][yCoord] = newNeighborDistance;
                                 Node neighbor = new Node(xCoord, yCoord, newNeighborDistance);
-                                //parentNodes.put(neighbor, place);
                                 neighbor.setParent(place);
                                 priorityQ.add(neighbor);
                             }
+
                         }
                     }
                 }
