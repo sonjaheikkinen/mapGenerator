8f85531f7a2c8468be865a85a38672e818c6925e
Sonja Heikkinen <sonja.t.heikkinen@hotmail.com>
2020-08-19 19:06:00 +0300

    Add javadoc to water generator

--- mapGenerator/src/main/java/mapgenerator/logic/WaterGenerator.java
+++ mapGenerator/src/main/java/mapgenerator/logic/WaterGenerator.java
@@ -1,6 +1,5 @@
 package mapgenerator.logic;
 
-import java.util.PriorityQueue;
 import java.util.Random;
 import mapgenerator.datastructures.BinaryHeap;
 import mapgenerator.datastructures.Node;
@@ -41,6 +40,12 @@ public class WaterGenerator {
         }
     }
     
+    /**
+     * Method adds rivers and lakes. The whole map is looped trough, and in every cell, a river might randomly be 
+     * started. The probability of the river starting in given location is based on height and moisture.
+     * @param heightmap Height values of the map as doubles
+     * @param moisture Moisture values of the map as doubles
+     */
     public void addRivers(double[][] heightmap, double[][] moisture) {
         for (int x = 0; x < heightmap.length; x++) {
             for (int y = 0; y < heightmap.length; y++) {
@@ -55,7 +60,17 @@ public class WaterGenerator {
             }
         }
     }
-
+    
+    /**
+     * Method generates river starting from a point given as parameter and stopping at the edge of the map or 
+     * when ending up in water. The river always goes to the direction of smallest height where there is still no water.
+     * If such neighbor cannot be found, the river continues approximately to the direction it was going until it hits water.
+     * @param riverX Current x coordinate of the river generation
+     * @param riverY Current y coordinate of the river generation
+     * @param heightmap Height values of the map as doubles
+     * @param earlierX X coordinate from which the river was coming
+     * @param earlierY Y coordinate from which the river was coming
+     */
     public void createRiver(int riverX, int riverY, double[][] heightmap, int earlierX, int earlierY) {
         while (!water[riverX][riverY]) {
             water[riverX][riverY] = true;
@@ -73,7 +88,14 @@ public class WaterGenerator {
             }
         }
     }
-
+    
+    /**
+     * Calculates new direction for the river based on the direction it was coming. If the earlier coordinate was smaller, 
+     * the coordinate is increased or kept the same and if it was larger, then the coordinate is decreased or kept the same. 
+     * @param earlierCoord Direction where the river is coming
+     * @param currentCoord Current coordinate of the river
+     * @return New coordinate
+     */
     public int setNewCoordinates(int earlierCoord, int currentCoord) {
         if (earlierCoord < currentCoord) {
             earlierCoord = currentCoord;
@@ -85,6 +107,14 @@ public class WaterGenerator {
         return currentCoord;
     }
 
+    /**
+     * Returns all neighbors of a coordinate as minimum binary heap. The current coordinates and coordinates that already 
+     * have water are not added to heap. 
+     * @param x
+     * @param y
+     * @param heightmap
+     * @return Neighbors as binary heap. 
+     */
     public BinaryHeap getNeighbors(int x, int y, double[][] heightmap) {
         BinaryHeap neighbors = new BinaryHeap();
         for (int xCoord = x - 1; xCoord <= x + 1; xCoord++) {
