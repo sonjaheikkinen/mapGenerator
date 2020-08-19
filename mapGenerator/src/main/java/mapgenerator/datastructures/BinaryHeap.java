2118e938112770f89cfb8c0f42e1e6adbc289bac
Sonja Heikkinen <sonja.t.heikkinen@hotmail.com>
2020-08-19 17:54:40 +0300

    Implement binary heap

--- mapGenerator/src/main/java/mapgenerator/datastructures/BinaryHeap.java
+++ mapGenerator/src/main/java/mapgenerator/datastructures/BinaryHeap.java
@@ -10,5 +10,84 @@ package mapgenerator.datastructures;
  * @author heisonja
  */
 public class BinaryHeap {
-    
+
+    private Node[] heap;
+    private int nodesInHeap;
+
+    //Juuri eli pienin tai suurin alkio on aina kohdassa 1.
+    //Jos solmu on kohdassa k, niin solmun vasen lapsi on kohdassa 2k, solmun oikea lapsi on kohdassa
+    //2k + 1 ja solmun vanhempi on kohdassa k/2.
+    public BinaryHeap() {
+        this.heap = new Node[9];
+        this.nodesInHeap = 0;
+    }
+
+    public void add(Node node) {
+        if (nodesInHeap == heap.length) {
+            increaseHeapSize();
+        }
+        int nodeIndex = nodesInHeap + 1;
+        heap[nodeIndex] = node;
+        nodesInHeap++;
+        while (heap[nodeIndex / 2].compareTo(node) == 1 && nodeIndex > 1) {
+            switchPlaces(nodeIndex / 2, nodeIndex);
+            nodeIndex = nodeIndex / 2;
+        }
+    }
+
+    public Node peek() {
+        return heap[1];
+    }
+
+    public Node poll() {
+        Node nodeToReturn = heap[1];
+        Node lastNode = heap[nodesInHeap];
+        heap[1] = lastNode;
+        int nodeIndex = 1;
+        while (true) {
+            moveNodeInHeap(nodeIndex);
+            nodeIndex = nodeIndex * 2;
+            if (nodeIndex >= nodesInHeap) {
+                break;
+            }
+        }
+        if (nodesInHeap <= heap.length / 4) {
+            decreaseHeapSize();
+        }
+        return nodeToReturn;
+    }
+
+    public void moveNodeInHeap(int nodeIndex) {
+        Node child1 = heap[nodeIndex * 2];
+        Node child2 = heap[nodeIndex * 2 + 1];
+        if (child1.compareTo(child2) == 1) {
+            switchPlaces(nodeIndex, nodeIndex * 2 + 1);
+        } else {
+            switchPlaces(nodeIndex, nodeIndex * 2);
+        }
+    }
+
+    public void switchPlaces(int index1, int index2) {
+        Node node1 = heap[index1];
+        Node node2 = heap[index2];
+        heap[index1] = node2;
+        heap[index2] = node1;
+    }
+
+    public void increaseHeapSize() {
+        Node[] newHeap = new Node[2 * heap.length];
+        for (int i = 0; i < heap.length; i++) {
+            newHeap[i] = heap[i];
+        }
+        heap = newHeap;
+    }
+
+    public void decreaseHeapSize() {
+        Node[] newHeap = new Node[heap.length / 2];
+        for (int i = 0; i < heap.length; i++) {
+            newHeap[i] = heap[i];
+        }
+        heap = newHeap;
+    }
+
 }
